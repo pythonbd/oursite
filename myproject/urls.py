300533aa5d166ec0addaff32442fb709980fdb49
jahurul25 <jahurul25@gmail.com>
2018-02-09 00:14:41 +0600

    admin panel customize code add in settings file

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.conf.urls import url, include
+admin.site.site_title = "Pythonbd Admin"
+admin.site.site_header = "Pythonbd Administrator"
 
 urlpatterns = [
     url('admin/', admin.site.urls),
